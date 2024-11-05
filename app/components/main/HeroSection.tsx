0a353049729e28d3aebb3d88e7aaa705ea353b57
skrodrigo <rodrigoa0987@gmail.com>
2024-11-05 10:55:59 -0300

    feat: add grid and selection for magicui

--- app/components/main/HeroSection.tsx
+++ app/components/main/HeroSection.tsx
@@ -1,16 +1,17 @@
 import { Button } from "@/components/ui/button";
+import RetroGrid from "@/components/ui/retro-grid";
 
 import React from "react";
 
 export default function HeroSection() {
 	return (
 		<div>
-			<div className="md:h-[700px] h-[#500]  w-full rounded-md flex md:items-center md:justify-center  bg-grid-white/[0.01] relative overflow-hidden">
+			<div className=" md:h-[700px] h-[#500]  w-full rounded-md flex md:items-center md:justify-center  bg-grid-white/[0.01] relative overflow-hidden">
 				<div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-					<h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-zinc-50 to-zinc-200 bg-opacity-50">
+					<h1 className="text-5xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-white">
 						Visualize <br /> Suas Métricas com Facilidade
 					</h1>
-					<p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+					<p className="mt-4 font-normal text-sm md:text-base text-neutral-300 max-w-lg text-center mx-auto">
 						Visualização de dados poderosa, implementação fácil e controle total
 						sobre suas métricas.
 					</p>
@@ -26,6 +27,7 @@ export default function HeroSection() {
 						</Button>
 					</div>
 				</div>
+				<RetroGrid />
 			</div>
 		</div>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,3 @@
-import { Button } from "@/components/ui/button";
-import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
-import {
-	BarChart3,
-	LineChart,
-	Settings,
-	Sparkles,
-	Star,
-	User,
-	Users,
-	Zap,
-} from "lucide-react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import AboutSection from "./components/main/AboutSection";
@@ -28,7 +10,7 @@ import TestemonialsSection from "./components/main/TestemonialsSection";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col min-h-screen bg-[#09090b] text-[#FFFFFF]">
+		<div className="flex flex-col min-h-screen selection:bg-zinc-900 selection:text-white">
 			<Header />
 			<main className="flex-1">
 				<HeroSection />
