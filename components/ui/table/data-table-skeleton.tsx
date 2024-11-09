f31ba0f6a890aca8487024c8da2bc9e21faab1b5
skrodrigo <rodrigoa0987@gmail.com>
2024-11-09 01:49:37 -0300

    fix: errors in prop key data-table-skeleton

--- components/ui/table/data-table-skeleton.tsx
+++ components/ui/table/data-table-skeleton.tsx
@@ -18,46 +18,44 @@ export function DataTableSkeleton({
 }) {
 	return (
 		<div className="w-full space-y-3 overflow-auto">
-			{searchableColumnCount > 0 || filterableColumnCount > 0 ? (
+			{(searchableColumnCount > 0 || filterableColumnCount > 0) && (
 				<div className="flex w-full items-center justify-between space-x-2 overflow-auto p-1">
 					<div className="flex flex-1 items-center space-x-2 space-y-4">
-						{searchableColumnCount > 0
-							? Array.from({ length: searchableColumnCount }).map(
-									(_, index) => (
-										<Skeleton key={i} className="h-10 w-[150px] lg:w-[250px]" />
-									),
-								)
-							: null}
-						{filterableColumnCount > 0
-							? Array.from({ length: filterableColumnCount }).map((_, i) => (
-									<Skeleton key={i} className="h-10 w-[70px] border-dashed" />
-								))
-							: null}
+						{Array.from({ length: searchableColumnCount }).map((_, index) => (
+							<Skeleton
+								key={`search-${searchableColumnCount}`}
+								className="h-10 w-[150px] lg:w-[250px]"
+							/>
+						))}
+						{Array.from({ length: filterableColumnCount }).map((_, index) => (
+							<Skeleton
+								key={`filter-${filterableColumnCount}`}
+								className="h-10 w-[70px] border-dashed"
+							/>
+						))}
 					</div>
-					{showViewOptions ? (
+					{showViewOptions && (
 						<Skeleton className="ml-auto hidden h-7 w-[70px] lg:flex" />
-					) : null}
+					)}
 				</div>
-			) : null}
+			)}
 			<div className="rounded-md border">
-				<ScrollArea className="h-[calc(80vh-220px)] rounded-md border  md:h-[calc(90dvh-220px)]">
+				<ScrollArea className="h-[calc(80vh-220px)] rounded-md border md:h-[calc(90dvh-220px)]">
 					<Table>
 						<TableHeader>
-							{Array.from({ length: 1 }).map((_, i) => (
-								<TableRow key={i} className="hover:bg-transparent">
-									{Array.from({ length: columnCount }).map((_, i) => (
-										<TableHead key={i}>
-											<Skeleton className="h-8 w-full" />
-										</TableHead>
-									))}
-								</TableRow>
-							))}
+							<TableRow>
+								{Array.from({ length: columnCount }).map(() => (
+									<TableHead key={`header-${columnCount}`}>
+										<Skeleton className="h-8 w-full" />
+									</TableHead>
+								))}
+							</TableRow>
 						</TableHeader>
 						<TableBody>
-							{Array.from({ length: rowCount }).map((_, i) => (
-								<TableRow key={i} className="hover:bg-transparent">
-									{Array.from({ length: columnCount }).map((_, i) => (
-										<TableCell key={i}>
+							{Array.from({ length: rowCount }).map((_, rowIndex) => (
+								<TableRow key={`row-${rowCount}`}>
+									{Array.from({ length: columnCount }).map(() => (
+										<TableCell key={`cell-${rowIndex}-${rowCount}`}>
 											<Skeleton className="h-8 w-full" />
 										</TableCell>
 									))}
